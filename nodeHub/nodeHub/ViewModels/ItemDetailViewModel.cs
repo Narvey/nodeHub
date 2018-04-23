145f1d0588fc8014406a1a42502c16eea48a1935
Narvey <nkharvey@outlook.com>
2018-04-22 19:53:26 -0600

    Add project files.

---
+++ nodeHub/nodeHub/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using nodeHub.Models;
+
+namespace nodeHub.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
